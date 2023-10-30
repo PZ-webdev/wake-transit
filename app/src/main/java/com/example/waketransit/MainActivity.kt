7a73889463ef33929b91d3b4fd4833667c327915
Patryk Zaprzała <patryk.zaprzala@gmail.com>
2023-10-30 20:13:14 +0100

    Init main screen

--- app/src/main/java/com/example/waketransit/MainActivity.kt
+++ app/src/main/java/com/example/waketransit/MainActivity.kt
@@ -1,15 +1,9 @@
 package com.example.waketransit
 
+import MainScreenPreview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.waketransit.ui.theme.WakeTransitTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WakeTransitTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                MainScreenPreview()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WakeTransitTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/waketransit/ui/screen/MainScreen.kt
@@ -0,0 +1,68 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.example.waketransit.R
+
+@Composable
+fun MainScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "WakeTransit",
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(bottom = 50.dp)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.directions),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.5f),
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Button(
+            onClick = { /* Dodaj obsługę kliknięcia */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp)
+        ) {
+            Text(text = "Start")
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Button(
+            onClick = { /* Dodaj obsługę kliknięcia */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp)
+        ) {
+            Text(text = "Finish")
+        }
+    }
+}
+
+@Composable
+@Preview
+fun MainScreenPreview() {
+    MainScreen()
+}
