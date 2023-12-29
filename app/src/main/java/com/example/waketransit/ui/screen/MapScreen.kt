9b8e68634473c5156ab29eb24f4e199a7bfb0748
Patryk Zaprzała <patryk.zaprzala@gmail.com>
2023-12-29 16:54:28 +0100

    Implementin map screen with search field

--- app/src/main/java/com/example/waketransit/ui/screen/MapScreen.kt
+++ app/src/main/java/com/example/waketransit/ui/screen/MapScreen.kt
@@ -5,22 +5,10 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -35,7 +23,7 @@ import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun MapScreen(navController: NavController) {
-    var uiSettings by remember { mutableStateOf(MapUiSettings()) }
+    val uiSettings: MapUiSettings by remember { mutableStateOf(MapUiSettings()) }
     val properties by remember { mutableStateOf(MapProperties(mapType = MapType.NORMAL)) }
     val location = LatLng(50.0295, 22.0067) // Rzeszów główny
     val cameraPositionState = rememberCameraPositionState {
@@ -49,7 +37,9 @@ fun MapScreen(navController: NavController) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         SearchField(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
             onValueChange = { /* Handle search query change here */ }
         )
 
